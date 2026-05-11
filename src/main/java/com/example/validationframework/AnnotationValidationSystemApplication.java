e9359fcacaf2112397ea6c1a6e225b33f0c25d1b
nicolasholanda <nikolas.nhm14@gmail.com>
2026-05-11 08:38:31 -0300

    Initial commit

---
+++ src/main/java/com/example/validationframework/AnnotationValidationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.validationframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationValidationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationValidationSystemApplication.class, args);
+    }
+}
