c413d568716131673c3fa7f64c266ae1075d8373
nicolasholanda <nikolas.nhm14@gmail.com>
2026-05-11 08:38:59 -0300

    Added custom validation annotations

---
+++ src/main/java/com/example/validationframework/annotation/NotNull.java
@@ -0,0 +1,12 @@
+package com.example.validationframework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNull {
+    String message() default "Field must not be null";
+}
